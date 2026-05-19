e63927028bf0c7dadc489fb0d717b656456f57fb
Alradenn Ibno <alradennibno@gmail.com>
2026-05-19 09:25:08 +0800

    add new packages for injecting css

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), cssInjectedByJsPlugin()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
